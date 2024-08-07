3cb88e801ef8ea4778c614739e0c8bebd97bfb63
Jinseong0216 <175368794+Jinseong0216@users.noreply.github.com>
2024-08-08 02:15:39 +0900

    Update 1219_길찾기.py
    uploaded from laptop

--- Algorithm/0807/1219_길찾기.py
+++ Algorithm/0807/1219_길찾기.py
@@ -1,32 +1,51 @@
 def DFS(start, end):
     v = start
-    stack = [v]
     visited = [0]*(99+1)
-#    visited[v] = 1
+    stack = []
     while True:
-        print(stack)
-        if v == end: 
+        if v == end:
             return 1
+
         for w in adjL[v]:
-            if visited[w] == 0:
-                stack.append(w)
-                visited[w] = 1
-                v = w
+            if visited[w] == 0: # 방문 x인 곳이면
+                stack.append(v) # 현 위치를 stack에 넣고
+                visited[v] = 1 # v를 방문했다고 표시
+                w = v
                 break
-        else:
-            if stack:
-                v = stack.pop()
-            else:
+        
+        else:                    # 모든 인접 노드들이 방문한 노드들이면
+            if stack:            # 뒤로 갈 곳이 있으면
+                w = stack.pop()  # w를 바로 뒤의 점으로
+            else:               # 뒤로 갈 곳이 없으면
                 return 0
-result = []
-for _ in range(10):
+            
+for t in range(10):
     tc, E = map(int, input().split())
     edges = list(map(int, input().split()))
-    adjL = [[]*(99+1)]
+    adjL = [[] for _ in range(99+1)]
     for i in range(E): adjL[edges[2*i]].append(edges[2*i+1])
 
-    ans = DFS(0, 99)
-    result.append(ans)
-#    print(f'#{tc} {ans}')
-print((result))
-#    print(adjL)
+    v = 0
+    visited = [0]*(99+1)
+    visited[v] = 1
+    stack = []
+    stack.append(v)
+    while True:
+        if v == 99:
+            ans = 1
+            break
+
+        for w in adjL[v]:
+            if visited[w] == 0:  # 방문 x인 곳이면
+                v = w            # 위치를 w로 변경
+                stack.append(v)  # w 위치를 stack에 넣고
+                visited[v] = 1   # w를 방문했다고 표시
+                break
+
+        else:                    # 모든 인접 노드들이 방문한 노드들이면
+            if stack:            # 뒤로 갈 곳이 있으면
+                v = stack.pop()  # w를 바로 뒤의 점으로
+            else:               # 뒤로 갈 곳이 없으면
+                ans = 0
+                break
+    print(f'#{tc} {ans}')
