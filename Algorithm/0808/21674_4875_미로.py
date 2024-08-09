6bb2fa1c68fe1152d7c31362131673b9e2a38769
최진성 <pnum4095@gmail.com>
2024-08-09 09:42:52 +0900

    uploaded from local

--- Algorithm/0808/21674_4875_미로.py
+++ Algorithm/0808/21674_4875_미로.py
@@ -1,19 +1,30 @@
 T = int(input())
-for t in range(1,T+1):
-    eqn = input().split()[::-1][1:]
-    ans, N, stack = 1, len(eqn), []
-    while eqn:
-        p = eqn.pop()
-        if p.isdecimal(): stack.append(p)        
-        elif len(stack) < 2: ans = 0
-        else:
-            x = int(stack.pop())
-            y = int(stack.pop())
-            if p == '+': stack.append(str(x+y))
-            elif p == '-': stack.append(str(x-y))
-            elif p == '*': stack.append(str(x*y))
-            elif p == '/': stack.append(str(x//y))
-            else: ans = 0
-    if len(stack) != 1: print(f'#{t} error')
-    if not ans: print(f'#{t} error')
-    else: print(f'#{t} {stack.pop()}')
+for t in range(1, T+1):
+    N = int(input())
+    
+    grid = [list(input().strip()) for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j] == '2':
+                start_x, start_y = i, j
+                break
+    visited = [[0]*N for _ in range(N)]
+    result = 0
+
+    def DFS(x, y):
+        global result, visited
+
+        if grid[x][y] == '3':
+            result = 1
+            return
+
+        visited[x][y] = 1
+        dxy = [[0, 1], [0,-1], [1,0], [-1,0]]
+        for dx, dy in dxy:
+            nx, ny = x +dx, y+dy
+            if 0 <= nx < N and 0 <= ny < N:
+                if grid[nx][ny] != '1' and visited[nx][ny] == 0:
+                    DFS(nx,ny)
+        return
+    DFS(start_x, start_y)
+    print(f'#{t} {result}')
