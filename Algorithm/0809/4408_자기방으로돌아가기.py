a023516759fbb2c516d3d47f2e8d73d4992763e6
최진성 <pnum4095@gmail.com>
2024-08-09 17:43:47 +0900

    uploaded from local

---
+++ Algorithm/0809/4408_자기방으로돌아가기.py
@@ -0,0 +1,10 @@
+1. 불가능 할 때 까지, 학생 채우는 함수.
+2. while 학생X: 1에서 정의한 함수; hour += 1
+3. for문 -> while -> 함수 (현hour가 min_hour보다 작은경우만)
+
+위치+1 ~ 도착지
+
+N = int(input())
+lst_student = [list(map(int, input().split())) for _ in range(N)]
+
+def move(arr, )
