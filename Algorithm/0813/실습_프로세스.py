453300b5bbd595e75b457f8ee856ee37d65f0cbb
최진성 <pnum4095@gmail.com>
2024-08-14 10:45:49 +0900

    uploaded from local

--- Algorithm/0813/실습_프로세스.py
+++ Algorithm/0813/실습_프로세스.py
@@ -11,3 +11,23 @@ def solution(priorities, location):
             if process[0] == location: break
         else: priorities_list.append(process)
     return answer
+
+
+def solution(priorities, location):
+    cnt = [0] * 11
+    priorities_list = []
+    answer = 0
+    for i in range(len(priorities)):
+        cnt[priorities[i]] += 1
+        priorities_list.append([i, priorities[i]])
+
+    while True:
+        process = priorities_list.pop(0)
+        if sum(cnt[process[1] + 1:]) == 0:
+            cnt[process[1]] -= 1
+            answer += 1
+            if process[0] == location:
+                break
+        else:
+            priorities_list.append(process)
+    return answer
---
+++ Algorithm/0814/10761_신뢰.py
@@ -0,0 +1,25 @@
+def move_to(start, end):
+    return (end - start) and (end-start)//abs(end-start)
+
+T = int(input())
+for t in range(1, 1+T):
+    info = input().split()
+    N, buttons, idx = int(info[0]), {'B': [], 'O': []}, {'B': 1, 'O': 1}
+    for i in range(N):
+        buttons[info[2*i+1]]= buttons[info[2*i+1]] + [int(info[2*i+2])]
+    i = time = cnt = 0
+    while cnt < N:
+        now, other = 'B', 'O'
+        if info[2*i+1] == 'O': now, other = 'O', 'B'
+
+        while idx[now] != buttons[now][0]:
+            idx[now] += move_to(idx[now], buttons[now][0])
+            if buttons[other]: idx[other] += move_to(idx[other], buttons[other][0])
+            time = time+1
+
+        buttons[now].pop(0)
+        if buttons[other]: idx[other] += move_to(idx[other], buttons[other][0])
+        time, cnt, i = time+1, cnt+1, i+1
+
+    print(f'#{t} {time}')
+
