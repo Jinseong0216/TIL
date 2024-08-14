331f6dcd4900bd91260a3258036c0f05d469f4b8
최진성 <pnum4095@gmail.com>
2024-08-14 16:23:39 +0900

    upoaded from local

--- Algorithm/0814/연습문제_미로2.py
+++ Algorithm/0814/연습문제_미로2.py
@@ -1,5 +1,5 @@
 # BFS로  풀기
-
+#
 # import sys
 # from collections import deque
 #
@@ -18,15 +18,19 @@
 #         grid.append(temp)
 #
 #     def bfs(grid, start, n, m):
-#         global ans
+#         global ans, cnt, cnt_2
 #         dij = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 #         visited = [[False]*m for _ in range(n)]
 #
 #         que = deque([start])
 #         visited[start[0]][start[1]] = True
 #
+#
 #         while que:
+#             cnt += 1
 #             i, j = que.popleft()
+#             print(que)
+#             if len(que) > cnt_2: cnt_2 = len(que)
 #
 #             for di, dj in dij:
 #                 ni, nj = i+di, j+dj
@@ -37,7 +41,7 @@
 #                     continue
 #                 if grid[ni][nj] == 1:
 #                     continue
-#
+#                 cnt += 1
 #                 que.append([ni, nj])
 #                 visited[ni][nj] = True
 #
@@ -46,15 +50,21 @@
 #                     return
 #
 #     ans = 0
+#     cnt = 0
+#     cnt_2 = 0
 #     bfs(grid, start, 100, 100)
-#     print(f'#{T} {ans}')
+#     print(f'#{T} {ans} {cnt} {cnt_2}')
 
 # DFS로  풀기
-
+import time
 import sys
-sys.stdin = open('input.txt', 'r')
 
-for t in range(1, 2):
+sys.stdin = open('input.txt', 'r')
+sys.setrecursionlimit(2648)
+
+start = time.time()
+print(start)
+for t in range(1, 11):
     T = int(input())
 
     start = end = -1
@@ -69,7 +79,7 @@ for t in range(1, 2):
 
     def dfs(vertex, grid, visited, n, m):
         global ans
-        print(vertex)
+
         if vertex == end:
             ans = 1
             return
@@ -77,7 +87,7 @@ for t in range(1, 2):
         dij = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
         for di, dj in dij:
-            ni, nj = i+di, j+dj
+            ni, nj = vertex[0]+di, vertex[1]+dj
             if 0 > ni or ni >= n or 0 > nj or nj >= m:
                 continue
             if visited[ni][nj]:
@@ -88,10 +98,13 @@ for t in range(1, 2):
             dfs([ni, nj], grid, visited, n, m)
             visited[ni][nj] = False
 
-    print(start)
-    print(end)
+
     visited = [[False] * 100 for _ in range(100)]
     visited[start[0]][start[1]] = True
     ans = 0
     dfs(start, grid, visited, 100, 100)
     print(f'#{T} {ans}')
+
+end = time.time()
+
+print(start - end)
