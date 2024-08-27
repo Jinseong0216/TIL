98b1cbf52582b6108c6d96bca106bd5f1b1bc60a
Jinseong <pnum4095@gmail.com>
2024-08-27 17:44:31 +0900

    uploaded from local

--- Algorithm/0827/01_tree/내 풀이/5176_이진탐색/sol.py
+++ Algorithm/0827/01_tree/내 풀이/5176_이진탐색/sol.py
@@ -1,7 +1,3 @@
-import sys
-sys.stdin = open('input.txt')
-
-
 for T in range(1, int(input())+1):
     N = int(input())
     root = 1
@@ -11,22 +7,20 @@ for T in range(1, int(input())+1):
         if 2*root+1 <= N: nodes[root][1] = 2*root+1
         root = root+1
 
-    order_list = [0]
+    order = 0
     node_data = [0]*(N+1)
 
     def inorder(root):
-        global order_list
+        global order_list, order
 
         if root == 0: return
 
         inorder(nodes[root][0])
-        order_list.append(order_list[-1]+1)
-        node_data[root] = order_list[-1]
+        order = order+1
+        node_data[root] = order
         inorder(nodes[root][1])
 
     root = 1
     inorder(root)
+    print(node_data)
     print(f'#{T}', node_data[root], node_data[N//2])
-
-
-
