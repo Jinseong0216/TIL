214b6ab408ce896eaaa8faa55e3136d3a0c4de83
Jinseong0216 <175368794+Jinseong0216@users.noreply.github.com>
2024-09-06 12:33:04 +0900

    [Silver I] Title: 나이트의 이동, Time: 1656 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+for T in range(1, int(input())+1):
+    N = int(input())
+    start = list(map(int, input().split()))
+    end = list(map(int, input().split()))
+    distance = [[-1] * N for _ in range(N)]
+
+    def bfs():
+        queue = deque([start])
+        distance[start[0]][start[1]] = 0
+        dij = [[-1, -2], [-1, 2], [1, -2], [1, 2], [-2, -1], [-2, 1], [2, -1], [2, 1]]
+
+        while queue:
+            i, j = queue.popleft()
+            for di, dj in dij:
+                ni, nj = i+di, j+dj
+                if ni<0 or nj<0 or ni>=N or nj>= N: continue
+                if distance[ni][nj] == -1:
+                    distance[ni][nj] = distance[i][j]+1
+                    queue.append((ni, nj))
+        return
+
+    bfs()
+    print(distance[end[0]][end[1]])
