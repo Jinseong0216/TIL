b70c5bc93c94911d875b1f1e8dfd6a93b5d8bb53
Jinseong0216 <175368794+Jinseong0216@users.noreply.github.com>
2024-09-10 10:06:12 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 13532 ms, Memory: 38472 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -0,0 +1,26 @@
+N, M = map(int, input().split())
+dic_info = {i: [0]*(N+1) for i in range(1, N+1)}
+for _ in range(M):
+    prior, subject = map(int, input().split())
+    dic_info[subject][prior] = 1
+
+ans = {i: 0 for i in range(1, N+1)}
+semester = 1
+
+while dic_info:
+    finish_lst = []
+    new_dic = {}
+    for subject, priority in dic_info.items():
+        if priority == [0]*(N+1):
+            finish_lst.append(subject)
+            ans[subject] = semester
+        else:
+            new_dic[subject] = priority
+
+    for subject in new_dic:
+        for finish in finish_lst:
+            new_dic[subject][finish] = 0
+    dic_info = new_dic
+    semester += 1
+
+print(*(ans.values()))
