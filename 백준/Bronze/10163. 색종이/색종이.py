96cfec1021c71d3e6203b81328f586f9c65f45d5
Jinseong0216 <175368794+Jinseong0216@users.noreply.github.com>
2024-09-10 11:37:30 +0900

    [Bronze I] Title: 색종이, Time: 336 ms, Memory: 204624 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,18 +1,9 @@
-# 10163번 색종이
+N, L = int(input()), []
 
-grid = [['0']*101 for _ in range(101)]
-N = int(input())
+for k in range(N):
+    a,b,c,d = map(int,input().split())
+    L += [set((a+i,b+j) for i in range(c) for j in range(d))]
+    for t in range(k):
+        L[t] = L[t] - L[k]
 
-for flag in range(1,1+N):
-    x, y, dx, dy = map(int, input().split())
-    for i in range(x, x + dx):
-        for j in range(y, y + dy):
-            grid[i][j] = (str(flag) + 'sep' + grid[i][j])[:len(str(flag))+3]
-
-areas = []
-Cnts = [0] + [0]*N
-for i in range(101):
-    for j in range(101):
-        Cnts[int(grid[i][j].split('sep')[0])] += 1
-
-for i in range(1,1+N): print(Cnts[i])
+for k in range(N): print(len(L[k]))
