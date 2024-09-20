308d6d4daf4f01bfcce88f915d61de8ad7017760
Jinseong <pnum4095@gmail.com>
2024-09-20 17:32:53 +0900

    캠퍼스내 학습한 내용들 나가기전 commit

--- SSAFY/django/02_django/03_model/99_offline/ws/todos/models.py
+++ SSAFY/django/02_django/03_model/99_offline/ws/todos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# 할 일 목록을 위한 모델을 정의
+
+# class Todo(models.Model):
+#     # 할 일을 저장
+#     work = models.CharField(max_length=10)
+#     # 완료 여부를 저장
+#     # 언제 만들었는지를 저장
+#     is_completed = models.BooleanField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
