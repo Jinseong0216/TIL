0fbcb8c8bf5921ba85adde30bc87d3f35164afc6
Jinseong0216 <175368794+Jinseong0216@users.noreply.github.com>
2024-11-07 16:39:00 +0900

    [Silver III] Title: N과 M (1), Time: 136 ms, Memory: 112652 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,18 +1,22 @@
-N, M = map(int, input().split())
-
-arr = list(range(1, N+1))
-visited = [True] + [False]*N
-seq = []
 def dfs(length = 0):
+    # 백 트래킹 조건
     if length == M: print(" ".join(seq)); return
-
-    for num in arr:
+    # 모든 숫자에 대해서 반복문 
+    for num in range(1, N+1):
+        # 이미 들고있는 숫자면 continue
         if visited[num]: continue
-        visited[num] = True
-        seq.append(str(num))
+        # 방문 체크; 숫자 추가
+        visited[num] = True; seq.append(str(num))
+        # 재귀 호출
         dfs(length+1)
-        seq.pop()
-        visited[num] = False
-    return
+        # 방문 체크 해체; 숫자 삭제
+        visited[num] = False; seq.pop()
 
-dfs()
+if __name__ == "__main__": 
+    N, M = map(int, input().split())
+    # 방문 체크용
+    visited = [False]*(N+1)
+    # 출력 전 수열을 담을 배열
+    seq = []
+    # 함수 호출
+    dfs()
