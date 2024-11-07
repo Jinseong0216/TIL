1678a13e252345fd3ddad523514b41a42c5ac1d3
Jinseong0216 <175368794+Jinseong0216@users.noreply.github.com>
2024-11-07 16:15:45 +0900

    [Gold IV] Title: N-Queen, Time: 10736 ms, Memory: 121348 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,20 +1,22 @@
-def is_possible(base, comparision):
-    if base[0] == comparision[0] or base[1] == comparision[1]: return False
-    if abs(base[0]-comparision[0]) == abs(base[1]-comparision[1]): return False
-    return True
-
-def dfs(level, available):
+def dfs(level, columns, diag1, diag2):
     global count
-    if level == N: count +=1; return        
-    for candidate in available:
-        if candidate[0] < level: continue
-        new_available = list(queen for queen in available if is_possible(candidate, queen) == True)
-        dfs(level+1, new_available)
-        if candidate[0] > level: return    
-    return
+    if level == N: 
+        count += 1; return
+    
+    for col in range(N):
+        if col in columns: continue
+        if (level - col) in diag1: continue
+        if (level + col) in diag2: continue 
+        
+        columns.add(col); diag1.add(level - col); diag2.add(level + col)
+        
+        dfs(level + 1, columns, diag1, diag2)
+        
+        columns.remove(col); diag1.remove(level - col); diag2.remove(level + col)
 
 N = int(input())
-available_space = [(i, j) for i in range(N) for j in range(N)]
 count = 0
-dfs(0, available_space)
+columns, diag1, diag2 = set(), set(), set()
+
+dfs(0, columns, diag1, diag2)
 print(count)
