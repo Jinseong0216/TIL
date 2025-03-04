55cf6463ca136a21f912095ce6f95abddbe1cd6f
Jinseong0216 <175368794+Jinseong0216@users.noreply.github.com>
2025-03-04 10:04:47 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 5047.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+    
+    def make_parenthesis(left_remained, used_left, used_right, step, bp):
+        nonlocal answer
+
+        if step == bp: answer +=1; return;
+        if left_remained < 0: return;
+        if used_left > n or used_right > n: return;
+        make_parenthesis(left_remained+1, used_left+1, used_right, step+1, bp)
+        make_parenthesis(left_remained-1, used_left, used_right+1, step+1, bp)
+        return
+    
+    make_parenthesis(0, 0, 0, 0, 2*n+1)
+    return answer//2
